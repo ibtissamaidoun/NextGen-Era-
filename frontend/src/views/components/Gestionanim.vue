e8236cc6919438e0d3e63a8932ed6b93c9a3c4f4
ibtissam aidoun <aidoun.ibtissam@etu.uae.ac.ma>
2024-05-20 18:31:55 +0200

    msg

--- frontend/src/views/components/Gestionanim.vue
+++ frontend/src/views/components/Gestionanim.vue
@@ -3,7 +3,7 @@
     <div class="card-header pb-0 px-3">
       <h4 class="mb-2 text-center">La gestion des animateurs</h4>
     </div>
-<<<<<<< HEAD
+
     <div class="card-body pt-4 p-3 text-center justify-content-center align-items-center"> 
       <table class="table table-bordered align-items-center">
         <thead>
@@ -49,7 +49,7 @@
           </tr>
         </tbody>
       </table>
-=======
+
     <div class="card-body px-0 pt-0 pb-2">
       <div class="table-responsive p-0">
         <table class="table align-items-center mb-0">
@@ -126,12 +126,11 @@
           </tbody>
         </table>
       </div>
->>>>>>> b0ff42eb0fd2d42c21dff1a6dd22cc85a9510d33
+
     </div>
     
   </div>
 </template>
-<<<<<<< HEAD
 
 <script>
 /* eslint-disable */
@@ -223,7 +222,7 @@ span {
   font-family: Georgia, 'Times New Roman', Times, serif;
 }
 </style>
-=======
+
 <script setup>
 //import { ref } from "vue";
 import axiosInstance from '@/main';
@@ -239,4 +238,4 @@ onBeforeMount(async() => {
 
 
 </script>
->>>>>>> b0ff42eb0fd2d42c21dff1a6dd22cc85a9510d33
+
