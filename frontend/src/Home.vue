8c3e2a300f615ee3caafd97735d394f5846f32bd
ibtissam <159814656+ibtissamaidoun@users.noreply.github.com>
2024-06-09 15:50:18 +0200

    Update Home.vue

--- frontend/src/Home.vue
+++ frontend/src/Home.vue
@@ -47,7 +47,7 @@ export default {
                    <a class="nav-link active" aria-current="page" href="#header">Accueil</a>
                </li>
                <li class="nav-item">
-                   <a class="nav-link" href="#services">Activités</a>
+                   <a class="nav-link" href="#services"></a>
                </li>
                <li class="nav-item">
                    <a class="nav-link" href="#contact">Contact Us</a>
